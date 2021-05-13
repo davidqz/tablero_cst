525004024a575e0b4f8df422cafe3d32c061e5e9
David Quiroz-Marin <davidqum@gmail.com>
2021-05-13 00:06:09 -0500

    Remover archivos auto generados .g.dart de github

--- lib/modelo_datos/modelo_datos.dart
+++ lib/modelo_datos/modelo_datos.dart
@@ -1,12 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
-
-/// This allows the `ModeloDatos` class to access private members in
-/// the generated file. The value for this is *.g.dart, where
-/// the star denotes the source file name.
 part 'modelo_datos.g.dart';
 
-/// An annotation for the code generator to know that this class needs the
-/// JSON serialization logic to be generated.
 @JsonSerializable()
 class ModeloDatos {
   List<Servicio> servicios;
